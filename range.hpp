e5692e35d6effd5c4e505a74b5714418c8d2ae52
Efrat Cohen <cohenefratoni747@gmail.com>
2019-05-08 16:53:11 +0300

    change range

--- range.hpp
+++ range.hpp
@@ -4,13 +4,20 @@ namespace itertools
 template <typename T>
 class range
 {
+    class iterator{
 public:
+    T _iter;
+    iterator(T iter);
+
      T _container1;
      T _container2;
 
  range(T rangeConst1 , T rangeCont2);
  T begin(T container1,T container2);
  T end( T container2, T container2);
+ 
+  void operator++(iter++);
 
+ }
 };
 };
