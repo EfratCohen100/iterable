bbbcfe6594f5b1e7eddda97f9e489d7cedd33cad
caron1211 <43571006+caron1211@users.noreply.github.com>
2019-05-10 15:32:19 +0300

    fix range

--- range.hpp
+++ range.hpp
@@ -4,85 +4,70 @@ namespace itertools
 template <typename T>
 class range
 {
-    private:
+  private:
+	T _begin;
+	T _end;
 
-    T _begin;
-    T _end;
+  public:
+	range(T _begin, T _end)
+	{
+		this->_begin = _begin;
+		this->_end = _end;
+	}
 
-public:
+	class iterator
+	{
+	  public:
+		T *_iter;
 
-range(T _begin,T _end)
-{
-    this -> _begin =_begin;
-    this-> _end = _end;
-}
+		iterator(T *_iter = nullptr) : _iter(_iter) {}
 
-class iterator{
-public:
-    T *_iter;
-    // T m_value;
-    // T m_next;
-
-    iterator(T* _iter = nullptr): _iter(_iter) {}
-   
-
-//      T _container1;
-//      T _container2;
-//  range(T rangeConst1 , T rangeCont2);
-//  T begin(T container1,T container2);
-//  T end( T container2, T container2);
-//   void operator++(iter++);
-
-		T& operator*() const {
+		T &operator*() const
+		{
 			return *_iter;
 		}
 
-		// T* operator->() const {
-		// 	return &(m_pointer->m_value);
-		// }
+		T *operator->() const
+		{
+			return &(_iter);
+		}
 
 		// ++i;
-		iterator& operator++() {
+		iterator &operator++()
+		{
 			++(*_iter);
 			return *this;
 		}
 
 		// i++;
 		// Usually iterators are passed by value and not by const& as they are small.
-		const iterator operator++(int) {
-			iterator tmp= *this;
+		const iterator operator++(int)
+		{
+			iterator tmp = *this;
 			(*_iter)++;
 			return tmp;
 		}
 
-		bool operator==(const iterator& rhs) const {
+		bool operator==(const iterator &rhs) const
+		{
 			return (*_iter) == (*rhs._iter);
 		}
 
-		bool operator!=(const iterator& rhs) const {
+		bool operator!=(const iterator &rhs) const
+		{
 			return (*_iter) != (*rhs._iter);
 		}
+	};
 
- };
-
-     public:
-
-    iterator begin() {
+  public:
+	iterator begin()
+	{
 		return iterator{&_begin};
 	}
-	
-	iterator end() {
+
+	iterator end()
+	{
 		return iterator{&_end};
 	}
-
-
- template<typename T> friend ostream& operator<< (ostream& os,  range<T>& type); ///??
 };
-
-    template<typename T> ostream& operator<< (ostream& os,  range<T>& type){
-        for(T i: type){
-            os << i ;
-        }
-        return os;
-    }
-};
+} // namespace itertools
