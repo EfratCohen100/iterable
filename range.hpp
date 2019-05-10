601528bb225f4dd22947ea02f7a87b908355c650
odelia <odeliamos0@gmail.com>
2019-05-10 11:46:59 +0300

    add iterator

--- range.hpp
+++ range.hpp
@@ -4,20 +4,69 @@ namespace itertools
 template <typename T>
 class range
 {
-    class iterator{
+    private:
+
+    T _begin;
+    T _end;
+
+public:
+
+range(T _begin,T _end);
+
+class iterator{
 public:
     T _iter;
+    T m_value;
+    T m_next;
     iterator(T iter);
 
-     T _container1;
-     T _container2;
+//      T _container1;
+//      T _container2;
+//  range(T rangeConst1 , T rangeCont2);
+//  T begin(T container1,T container2);
+//  T end( T container2, T container2);
+//   void operator++(iter++);
 
- range(T rangeConst1 , T rangeCont2);
- T begin(T container1,T container2);
- T end( T container2, T container2);
- 
-  void operator++(iter++);
+		T& operator*() const {
+			_iter=m_value;
+            return _iter;
+		}
 
- }
+		// T* operator->() const {
+		// 	return &(T _iter;->m_value);
+		// }
+
+		
+		iterator& operator++() {
+			_iter=m_next;
+			return *this;
+		}
+
+		const iterator operator++(int) {
+			iterator tmp= *this;
+			_iter=m_next;
+			return tmp;
+		}
+
+		bool operator==(const iterator& rhs) const {
+			return _iter == rhs._iter;
+		}
+
+		bool operator!=(const iterator& rhs) const {
+			return _iter != rhs._iter;
+		}
+
+ };
+
+     public:
+
+    iterator begin() {
+		return iterator{_begin};
+	}
+	
+	iterator end() {
+		return iterator{_end};
+	}
+
 };
-};
+}
