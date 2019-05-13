ef7663fb92dbef85fbb60d0860d5658c64f4a044
caron1211 <43571006+caron1211@users.noreply.github.com>
2019-05-13 16:39:52 +0300

    fix powerset

--- powerset.hpp
+++ powerset.hpp
@@ -1,83 +1,163 @@
 #pragma once
 
 
+
+
+
 namespace itertools
+
 {
+
 template <typename T>
 
+
+
 class powerset
+
 {
 
+
+
    private:
+
 	T _begin;
-	T _end;
+
+
 
   public:
+
 	
-	powerset <T> (T _begin,T _end):_begin(_begin) , _end(_end){}
+
+	powerset <T> (T _begin):_begin(_begin) {}
+
+
+
 
 
 	class iterator
+
 	{
+
 	  private:
-		T _iter;
+
+		typename T::iterator _iter ;
+
+
 
     public:
-		iterator(T _iter ) : _iter(_iter) {}
 
-		auto &operator*() const
+		iterator(typename T::iterator _iter ) : _iter(_iter) {}
+
+
+
+		auto operator*() const
+
 		{
+
 			
+
 				return *_iter;
+
 			
+
 		}
+
+
 
 		// auto *operator-> () const
+
 		// {
+
 		
+
 		// 		return &_iter;
+
 		
+
 		// }
 
+
+
 		// ++i;
+
 		iterator &operator++()
+
 		{
-			++(_iter);
+
+
+
 			return *this;
+
 		}
+
+
 
 		// i++;
+
 		// Usually iterators are passed by value and not by const& as they are small.
+
 		const iterator operator++(int)
+
 		{
+
 			iterator tmp = *this;
+
 			(_iter)++;
+
 			return tmp;
+
 		}
+
+
 
 		bool operator==(const iterator &rhs) const
+
 		{
-			return (_iter) == (rhs._iter);
+
+			return false;
+
 		}
+
+
 
 		bool operator!=(const iterator &rhs) const
+
 		{
-			return (_iter) != (rhs._iter);
+
+			return false;
+
 		}
+
 	};
 
+
+
 public:
+
 	iterator begin()
+
 	{
+
 		return powerset<T>::iterator(_begin.begin());
+
 	}
 
+
+
 	iterator end()
+
 	{
-		return  powerset<T>::iterator(_end.end());
+
+		return  powerset<T>::iterator(_begin.end());
+
 	}
+
+
+
 
 
 };
+
+
 
 };
