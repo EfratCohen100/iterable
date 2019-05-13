5e80d5c27222d0a32e23b6b69f0940b352ae9878
caron1211 <43571006+caron1211@users.noreply.github.com>
2019-05-13 13:14:24 +0300

    fix chain

--- chain.hpp
+++ chain.hpp
@@ -1,102 +1,78 @@
 #pragma once
-#include "range.hpp"
 
 namespace itertools
 {
-template <typename T,typename U>
+template <typename T, typename U>
 class chain
 {
 
 private:
+private:
+	T c1;
+	U c2;
 
-    T beginT;
-    T endT;
-    U beginU;
-    U endU;
-
-  public:
-	// chain(T beginT,T endT,U beginU, U endU)
-	// {
-	// 	this->beginT = beginT;
-	// 	this->endT = endT;
-  //       this->beginU = beginU;
-	// 	this->endU = endU;
-	// }
-
-    chain <T,U>(T typeT,U typeU) : beginT(typeT) , beginU(typeU){}
-    
+public:
+	chain(T c1, U c2) : c1(c1) , c2(c2) {}
 
 	class iterator
 	{
-	  
-		private:
-      typename T::iterator iterBeginT;
-      typename T::iterator iterEndT;
-      typename U::iterator iterBeginU;
-      typename U::iterator iterEndU;
 
-				public:
+	private:
+		typename T::iterator itr1;
+		typename U::iterator itr2;
 
-		iterator(typename T::iterator iter1BeginT,typename T::iterator iter1EndT, typename U::iterator iter2BeginU,typename U::iterator iter2EndU)
-		 : iterBeginT(iter1BeginT),iterEndT(iter1EndT),iterBeginU(iter2BeginU),iterEndU(iter2EndU) {}
+		
 
-		T &operator*() const/////////////////////////////////////////////////////////////////////////////////
+	public:
+		iterator(typename T::iterator itr1, typename U::iterator itr2) : itr1(itr1) , itr2(itr2) {}
+
+		auto &operator*() const
 		{
-			return iterBeginT;
+			
+				return *itr1;
+			
 		}
 
-		T *operator->() const//////////////////////////////////////////////////////////////////////////////////
-		{
-			return &(iterBeginT);
-		}
+		// auto *operator-> () const
+		// {
+		
+		// 		return &itr1;
+		
+		// }
 
 		// ++i;
 		iterator &operator++()
 		{
-            // if(*iterBeginT != *iterEndT)
-            // {
-            //     ++(*iterBeginT);
-            // }
-            // else
-            // {
-            //    if(*iterBeginU != *iterEndU)
-            //    ++(*iterBeginU);
-            // }
 			return *this;
 		}
 
- 		// i++;
-		const iterator operator++(int)//////////////////////////////////////////////////////////////////
+		// i++;
+		// Usually iterators are passed by value and not by const& as they are small.
+		const iterator operator++(int)
 		{
-			iterator tmp = *this;
-			(iterBeginT)++;
-			return tmp;
+			return *this;
 		}
 
 		bool operator==(const iterator &rhs) const
 		{
-			return true;
+			return false;
 		}
 
-		bool operator!=(const iterator &rhs) const  //////////////////////////////////////////////////////////////
+		bool operator!=(const iterator &rhs) const
 		{
-			return true;
+			return false;
 		}
- 	};
+	};
 
-  public:
+public:
 	iterator begin()
 	{
-		return chain<T,U>::iterator{beginT};////////////////////////////////////////////////////////
+		return chain<T,U>::iterator(c1.begin(), c2.begin());
 	}
 
 	iterator end()
 	{
-		return chain<T,U>::iterator{endU};///////////////////////////////////////////////////////////
+		return  chain<T,U>::iterator(c1.end(), c2.end());
 	}
-
-
-
-
 };
-}
+} // namespace itertools
