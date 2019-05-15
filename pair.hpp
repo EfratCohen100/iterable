1cd14cd4b2bd7b4b8f651b7689954e205b785302
car <caron1211@gmail.com>
2019-05-15 17:39:32 +0300

    add pair.hpp

---
+++ pair.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cassert>
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ostream;
+using std::string;
+
+template <typename T1, typename T2> 
+struct pair {
+	T1 first;
+	T2 second;
+};
+
+
+template <typename T1, typename T2> 
+ostream& operator<< (ostream& out, 
+const pair<T1,T2>& thepair) {
+	out << thepair.first << "," << thepair.second;
+	return out;
+}
